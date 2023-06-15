8d6b8c1b7dcadb53726aa6589b90c645f54052a9
cole-musili <colemiles206@gmail.com>
2023-06-15 15:18:37 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main - a programe rhat uses puts to print a line
+* Return: 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
