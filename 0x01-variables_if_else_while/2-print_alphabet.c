2ed0c91f9cc07689ca7b827802184dfd7859e215
cole-musili <colemiles206@gmail.com>
2023-06-16 17:20:36 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Printing lowercase alphabets
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
