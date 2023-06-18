2fd7e86fadcbc590cf1a24cdcb0f23940f984087
cole-musili <colemiles206@gmail.com>
2023-06-18 14:20:25 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+
+	printf("\n");
+	return (0);
+}
+
