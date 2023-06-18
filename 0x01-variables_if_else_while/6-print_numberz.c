9aa6b861117165a034f216b43f6dc7cceed00f5f
cole-musili <colemiles206@gmail.com>
2023-06-18 14:51:00 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+	}
+
+	putchar('\n');
+	return (0);
+}
+
