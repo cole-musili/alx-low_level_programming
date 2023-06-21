2276ec5e8d0ae2755f0b7ea8b15d8021fef85e9a
cole-musili <colemiles206@gmail.com>
2023-06-21 12:31:34 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
- * Description: Prints "putchar" using _putchar
+ * main - Entry point
  *
- * Return: 0
+ * Description: Prints "putchar" using _putchar function
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -16,6 +18,7 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
 
